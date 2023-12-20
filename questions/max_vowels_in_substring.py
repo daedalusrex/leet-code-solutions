9a5a4634f6f2b47355c3fdc5bd023b25997e1488
Jaco Navani <jaredcombs5@gmail.com>
2023-12-19 23:06:09 -0800

    These work! but they all made incredibly similar errors, however they were able to fix performance when prompted

--- questions/max_vowels_in_substring.py
+++ questions/max_vowels_in_substring.py
@@ -1,5 +1,3 @@
-
-
 def max_vowels_mostly_generated(s: str, k: int) -> int:
     """
     Calculates the maximum number of vowels in a substring of length k in a given string s.
@@ -12,7 +10,7 @@ def max_vowels_mostly_generated(s: str, k: int) -> int:
     Returns:
         int: The maximum number of vowels in a substring of length k.
     """
-    vowels = {'a', 'e', 'i', 'o', 'u'}
+    vowels = {"a", "e", "i", "o", "u"}
     max_vowel = 0
     i_range = range(len(s) - k + 1) if len(s) >= k else range(1)
     for i in i_range:
@@ -24,3 +22,38 @@ def max_vowels_mostly_generated(s: str, k: int) -> int:
         max_vowel = max(max_vowel, curr_vowels)
     return max_vowel
 
+
+def jetbrains_version(s: str, k: int) -> int:
+    vowels = {'a', 'e', 'i', 'o', 'u'}
+
+    if k > len(s):
+        return sum(char in vowels for char in s)
+
+    curr_vowels = sum(s[i] in vowels for i in range(k))
+    max_vowel = curr_vowels
+    for i in range(k, len(s)):
+        if s[i - k] in vowels:
+            curr_vowels -= 1
+        if s[i] in vowels:
+            curr_vowels += 1
+        max_vowel = max(max_vowel, curr_vowels)
+    return max_vowel
+
+
+
+def chatgpt_version(s: str, k: int) -> int:
+    vowels = {"a", "e", "i", "o", "u"}
+
+    # Calculate number of vowels in the first window
+    curr_vowels = sum(s[i] in vowels for i in range(k))
+
+    max_vowel = curr_vowels
+    for i in range(k, len(s)):
+        # Subtract the count of the vowel at the end of the previous window
+        if s[i - k] in vowels:
+            curr_vowels -= 1
+        # Add the count of the vowel at the start of the new window
+        if s[i] in vowels:
+            curr_vowels += 1
+        max_vowel = max(max_vowel, curr_vowels)
+    return max_vowel
